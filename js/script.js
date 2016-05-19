92f4ff1f4d92492fb2f9aeae56606b53f35d0c19
Hani <msowe.feha@gmail.com>
2016-05-19 02:05:38 -0600

    second init

--- js/script.js
+++ js/script.js
@@ -1,153 +1,130 @@
-function clearInput() {
-    document.getElementById("inputText").value = '';
-    document.getElementById("inputText").style.textTransform = "none";
-    document.getElementById("status").innerHTML = '';
-    document.getElementById("charcounts").innerHTML = 0;
-    document.getElementById("wordcounts").innerHTML = 0;
-}
+function DoThis(manipulation){
 
-function inputCounter() {
-    document.getElementById("status").innerHTML = '';
-    var inputText = $.trim(document.getElementById("inputText").value.replace( /  +/g, ' ' ));
-    var inputLength = inputText.length;
-    document.getElementById("charcounts").innerHTML = inputLength;
-    if (inputLength) {
-      document.getElementById("wordcounts").innerHTML = inputText.split(' ').length;
+    var inputText = document.getElementById("inputText").value;
+    if (inputText) {
+      switch(manipulation) {
+        case 'lowerCase':
+            document.getElementById("inputText").style.textTransform = "lowercase";
+            document.getElementById("status").style.color = 'blue';
+            document.getElementById("status").innerHTML = 'lowercase all Done!';
+        break;
+
+        case 'upperCase':
+            document.getElementById("inputText").style.textTransform = "uppercase";
+            document.getElementById("status").style.color = 'blue';
+            document.getElementById("status").innerHTML = 'UPPERCASE ALL Done!';
+        break;
+
+        case 'titleCase':
+            document.getElementById("inputText").style.textTransform = "capitalize";
+            document.getElementById("status").style.color = 'blue';
+            document.getElementById("status").innerHTML = 'Title Case All Done!';
+        break;
+
+        case 'clearStatus':
+            document.getElementById("status").innerHTML = '';
+        break;
+
+        case 'clearInput':
+            document.getElementById("inputText").value = '';
+            document.getElementById("inputText").style.textTransform = "none";
+            document.getElementById("inputText").focus();
+            document.getElementById("charcounts").innerHTML = 0;
+            document.getElementById("wordcounts").innerHTML = 0;
+            document.getElementById("status").style.color = 'b3b3b3';
+            document.getElementById("status").innerHTML = 'Cleared!';
+        break;
+
+        case 'copyInput':
+            var selector = document.querySelector('#inputText');
+            selector.select();
+            try {
+            var successful = document.execCommand('copy');
+            if (successful) {
+                document.getElementById("status").style.color = 'green';
+                document.getElementById("status").innerHTML = 'Copied Successfully!';
+              } else {
+                document.getElementById("status").style.color = 'red';
+                document.getElementById("status").innerHTML = 'Unable to copy!';
+            }
+            } catch (err) {
+                console.log('Oops, unable to copy');
+            }
+        break;
+
+        case 'countMatches':
+            var matchText = document.getElementById("match").value;
+            if (matchText) {
+                var matches = inputText.split(matchText).length - 1;
+                document.getElementById("status").style.color = 'blue';
+                document.getElementById("status").innerHTML = 'Found <b>' + matches + '</b> matches!';
+                } else {
+                document.getElementById("status").style.color = 'red';
+                document.getElementById("status").innerHTML = 'What would you like to count?';
+            }
+        break;
+
+        case 'removeSomething':
+
+            var toRemove = document.getElementById("toRemove").value;
+            if (toRemove) {
+                var found = inputText.split(toRemove).length - 1;
+
+                cleared = inputText.split(toRemove).join('');
+                document.getElementById("inputText").value = cleared;
+                inputCounter();
+                document.getElementById("status").style.color = 'blue';
+                document.getElementById("status").innerHTML = 'Found and removed <b>' + found + '</b> times!';
+
+            } else {
+                document.getElementById("status").style.color = 'red';
+                document.getElementById("status").innerHTML = 'What would you like to remove?';
+            }
+        break;
+
+        case 'replaceSomething':
+            var replaceThis = document.getElementById("replaceThis").value;
+            var withThis = document.getElementById("withThis").value;
+            if (replaceThis && withThis) {
+                replaced = inputText.split(replaceThis).join(withThis);
+                document.getElementById("inputText").value = replaced;
+                var replaced = inputText.split(replaceThis).length - 1;
+                inputCounter();
+                document.getElementById("status").style.color = 'blue';
+                document.getElementById("status").innerHTML = 'Replaced <b>' + replaced + '</b> times!';
+            } else {
+                document.getElementById("status").style.color = 'red';
+                document.getElementById("status").innerHTML = 'What would you like to replaces?';
+            }
+        break;
+
+        default:
+
+        } // end of: switch (manipulation)
     } else {
-      document.getElementById("wordcounts").innerHTML = 0;
+      document.getElementById("status").innerHTML = 'Enter some text..';
+
     }
-}
 
-////
-
-function copyInput() {
-  if (document.getElementById("inputText").value) {
-  var copyTextarea = document.querySelector('#inputText');
-    copyTextarea.select();
-    try {
-      var successful = document.execCommand('copy');
-      if (successful) {
-        document.getElementById("status").style.color = 'green';
-        document.getElementById("status").innerHTML = 'Copied Successfully!';
-      }
-       else {
-         document.getElementById("status").style.color = 'red';
-         document.getElementById("status").innerHTML = 'Unable to copy!';
-       }
-
-    } catch (err) {
-      console.log('Oops, unable to copy');
-    }
-  } else {
-    document.getElementById("status").style.color = 'blue';
-    document.getElementById("status").innerHTML = 'There is nothing to copy!';
-    }
-    fadeoutStatus();
-}
-
-///
-
-function lowerCase(){
-  var lowerCaseit = document.getElementById("inputText").value;
-  if (lowerCaseit) {
-  document.getElementById("inputText").style.textTransform = "lowercase";
-  document.getElementById("status").style.color = 'blue';
-  document.getElementById("status").innerHTML = 'lowercase all Done!';
-  fadeoutStatus();
-  }
-}
-
-function upperCase(){
-  var upperCaseit = document.getElementById("inputText").value;
-  if (upperCaseit) {
-  document.getElementById("inputText").style.textTransform = "uppercase";
-  document.getElementById("status").style.color = 'blue';
-  document.getElementById("status").innerHTML = 'UPPERCASE ALL Done!';
-  fadeoutStatus();
-  }
-}
-
-function TitleCase(){
-  var toTitleCase = document.getElementById("inputText").value;
-  if (toTitleCase) {
-    document.getElementById("inputText").style.textTransform = "capitalize";
-    document.getElementById("status").style.color = 'blue';
-    document.getElementById("status").innerHTML = 'Title Case All Done!';
-    fadeoutStatus();
-    }
-}
-
-function capFirst() {
-  var capFirst = document.getElementById("inputText").value;
-
-    var fixed = capFirst.replace(/.+?[\.\?\!](\s|$)/g, function (txt) {
-          return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-      });
-      document.getElementById("inputText").innerHTML = fixed;
+    // document.getElementById("status").innerHTML = 'Case: ' + manipulation;
 
 }
-////
-
-function countOccurence(){
-  var matchText = document.getElementById("match").value;
-  if (matchText) {
-      var inputText = document.getElementById("inputText").value;
-      var matches = inputText.split(matchText).length - 1;
-      document.getElementById("status").style.color = 'blue';
-      document.getElementById("status").innerHTML = 'Found <b>' + matches + '</b> matches!';
-      return matches;
-  } else {
-    document.getElementById("status").innerHTML = 'Enter something to find out!';
-    fadeoutStatus();
-  }
-}
-
-function removeSomething(){
-  var toRemove = document.getElementById("toRemove").value;
-   if (toRemove) {
-    var inputText = document.getElementById("inputText").value;
-    var found = inputText.split(toRemove).length - 1;
-
-    cleared = inputText.split(toRemove).join('');
-    document.getElementById("inputText").value = cleared;
-    inputCounter();
-    document.getElementById("status").style.color = 'blue';
-    document.getElementById("status").innerHTML = 'Found and removed <b>' + found + '</b> times!';
-
-  }
-else {
-  document.getElementById("status").innerHTML = 'Enter what would you like to remove!';
-  fadeoutStatus();
-  }
-}
-
-function replaceSomething(){
-  var replaceThis = document.getElementById("replaceThis").value;
-  var withThis = document.getElementById("withThis").value;
-  if (replaceThis && withThis) {
-    var inputText = document.getElementById("inputText").value;
-    replaced = inputText.split(replaceThis).join(withThis);
-    document.getElementById("inputText").value = replaced;
-    var foundReplaceThis = inputText.split(replaceThis).length - 1;
-    inputCounter();
-    document.getElementById("status").style.color = 'blue';
-    document.getElementById("status").innerHTML = 'Replaced <b>' + foundReplaceThis + '</b> times!';
 
 
-  }
-  else {
-    document.getElementById("status").innerHTML = 'Fill out what do you want to replace!';
-    fadeoutStatus();
-  }
-}
 
-function clearStatus() {
-  document.getElementById("status").innerHTML = '';
- }
 
- function fadeoutStatus(){
-   $("#status").fadeOut(2800, function() {
+
+
+
+
+ function inputCounter() {
      document.getElementById("status").innerHTML = '';
-     $("#status").show();
-     });
+     var inputText = $.trim(document.getElementById("inputText").value.replace( /  +/g, ' ' ));
+     var inputLength = inputText.length;
+     document.getElementById("charcounts").innerHTML = inputLength;
+     if (inputLength) {
+       document.getElementById("wordcounts").innerHTML = inputText.split(' ').length;
+     } else {
+       document.getElementById("wordcounts").innerHTML = 0;
+     }
  }
